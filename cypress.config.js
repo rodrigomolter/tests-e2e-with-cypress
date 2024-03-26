fb8263f2d4724b6f70f1362bf30a7111870dda6f
Rodrigo Molter <rodrigo.molter@gmail.com>
2024-03-26 10:38:47 -0300

    fix: added more default timeout time

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   projectId: '95bkg6',
   requestTimeout: 10000,
+  defaultCommandTimeout: 10000,
   e2e: {
     baseUrl: 'https://notes-serverless-app.com',
     env: {
